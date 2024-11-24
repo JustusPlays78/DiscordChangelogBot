ec9bd5700d469d2371af9daf5e0444b751c08acc
Julscha <info@julscha.de>
2024-11-24 18:44:32 +0100

    made API working

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import * as dotenv from 'dotenv';
-import { Client, GatewayIntentBits, PermissionsBitField, EmbedBuilder, MessageReaction } from 'discord.js';
-import { fetchText } from './text.js';
+import { Client, GatewayIntentBits } from 'discord.js';
+import {fetchText, getChanges} from './text.js';
+import {createChangelogEmbed} from "./embed builder.js";
+import express from 'express';
 
 
 dotenv.config();
 const token = process.env.CLIENT_AUTH_TOKEN;
+//const accesstoken = process.env.GITHUB_ACCESS_TOKEN;
 
+const app = express();
+app.use(express.json());
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -17,12 +22,56 @@ const client = new Client({
 client.on('ready', async () => {
     console.log('Ready!');
     try {
-        const result = await fetchText("https://raw.githubusercontent.com/JustusPlays78/ES_AIRAC_PACKAGE_UPDATER/refs/heads/main/README.md");
-        console.log('result:', result);
+        console.log('client ready');
     } catch (error) {
         console.error(error);
     }
 })
+
+/*app.post('/changelog', async (message) => {
+    let result = await fetchText("https://tee.intjulscha.de/Julscha/changelog/raw/branch/main/changelog_november.md", accesstoken);
+    console.log(result);
+    result = await getChanges(result);
+    const embed = createChangelogEmbed();
+    message.channel.send({ embeds: [embed] });
+});*/
+
+app.post('/add-role', async (req, res) => {
+    const { guildId, userId, roleId } = req.body;
+
+    const guild = await  client.guilds.fetch(guildId);
+    const members = await guild.members.fetch(userId);
+
+    try {
+        await members.roles.add(roleId);
+        res.status(200).send('Role added successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('error');
+    }
+});
+
+app.post('/remove-role', async (req, res) => {
+    const { guildId, userId, roleId } = req.body;
+    const guild = await  client.guilds.fetch(guildId);
+    const members = await guild.members.fetch(userId);
+
+    try {
+        await members.roles.remove(roleId);
+        res.status(200).send('Role removed successfully.');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('error');
+    }
+});
+
+const PORT  = process.env.APIPORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
+
+
+
 
 client.login(token);
 
